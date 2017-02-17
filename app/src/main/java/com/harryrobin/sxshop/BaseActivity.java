23228fb02380a5592352eb06ca745bc1a59f7028
HarryRobin <HarryRobin2011@gmail.com>
2017-02-17 17:27:03 +0800

    20170217

---
+++ app/src/main/java/com/harryrobin/sxshop/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.harryrobin.sxshop;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mac on 2017/2/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
